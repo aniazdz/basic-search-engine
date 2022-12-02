dc2dac68c279b68c6bb45bedb7844b71806ac580
Sean Ji <97076993+SeanJi213@users.noreply.github.com>
2022-12-02 00:52:33 -0800

    add the query log for analysis

--- app.py
+++ app.py
@@ -6,17 +6,19 @@ from datetime import datetime
 # sourced from https://flask.palletsprojects.com/en/2.2.x/tutorial/
 # -- https://www.digitalocean.com/community/tutorials/how-to-make-a-web-application-using-flask-in-python-3
 
-app = Flask('__name__')
+app = Flask('__name__') 
 
 @app.route('/', methods=['POST', 'GET'])
 
 def index():
+    query_log_file = open('query_log.txt', 'a')
     query = Query(Indexer())
     if request.method == 'POST':
         search_content = request.form['content']
         try:
             if search_content == '':
                 pass
+            query_log_file.write(f"Query: {search_content}\n")
             start_time = time.time()
             query.get_query_tokens(search_content)
             query.ranking_retrieval()
@@ -30,10 +32,16 @@ def index():
             results = query.result()
             end_time = time.time()
             search_time = end_time - start_time
+            query_log_file.write(f"Search time: {(search_time * 1000):.2f}ms\n")
+            for result in results:
+                query_log_file.write(f"{result}\n")
+            query_log_file.write('\n\n')
             print(f"Search Time: {(search_time * 1000):.2f}ms")
             return render_template('index.html', results=results, search_content=search_content)
     else:
+        query_log_file.close()
         return render_template('index.html') #, results=results, search_content=search_content)
     
 if __name__=='__main__':
     app.run(debug=True)
+    
